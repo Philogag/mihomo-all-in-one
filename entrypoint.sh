06a5649798a9657d284ba34f9af3fcdefb6fe7e3
philogag <philogag@qq.com>
2026-05-01 14:56:18 +0800

    add extra custom daemon support

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,10 @@ if [[ "x${SUBSCRIBE_URL}" != "x" ]]; then
   /opt/subscribe-daemon.sh &
 fi
 
+if [[ "x/opt/custom/custom-daemon.sh" != "x" ]]; then
+  /opt/custom/custom-daemon.sh &
+fi
+
 /opt/rebuild-config.sh
 
 /mihomo
